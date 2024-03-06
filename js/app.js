48f8f7bfda8d2d57e025b0965679fcaf3c9bd239
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-06 16:04:49 +0100

    Add: Bonus 1, Bonus 2

--- js/app.js
+++ js/app.js
@@ -9,21 +9,44 @@ for (let i= 0; i < n; i++) {
     //scrivo le variabili per identificare quali siano i numeri multipli di 3 e di 5 
     const multiplo3 = num % 3
     const multiplo5 = num % 5
+
+    //----creo un container nel dom 
+    //------seleziono il mio container dal dom 
+    const chess = document.querySelector('.chess') //object
+    
     //--Se sono multipli sia di 3 che di 5 stampo "FizzBuzz" al posto del num
     if (multiplo3 === 0 && multiplo5 === 0) {
         
         console.log("FizzBuzz")
 
+        //----aggiungo un elemento html con il numero corretto dentro l'elemento
+        const cell = `<div class="square flex align-items-center blue"><p>${"FizzBuzz"}</p></div>`
+        chess.innerHTML += cell
+
         //--Se sono multipli di 3 stampo "Fizz" al posto del num
     } else if (multiplo3 === 0) {   
         console.log("Fizz");
+
+        //----aggiungo un elemento html con il numero corretto dentro l'elemento
+        const cell = `<div class="square flex align-items-center red"><p>${"Fizz"}</p></div>`
+        chess.innerHTML += cell
 
         //--Se sono multipli di 5 stampo "Buzz" al posto del num
     } else if (multiplo5 === 0) {
         console.log("Buzz");
 
+        //----aggiungo un elemento html con il numero corretto dentro l'elemento
+        const cell = `<div class="square flex align-items-center yellow"><p>${"Buzz"}</p></div>`
+        chess.innerHTML += cell
+
         //--Se no stampo il numero
     } else {
         console.log(num)
+
+        //----aggiungo un elemento html con il numero corretto dentro l'elemento
+        const cell = `<div class="square flex align-items-center white"><p>${num}</p></div>`
+        chess.innerHTML += cell
+        
     }
 }
+
