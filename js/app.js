9b1676a013e6454c76a67449031c56434bebd553
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-06 14:55:54 +0100

    Add: FizzBuzz ex

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//Scrivere un programma che stampi in console i numeri da 1 a 100
+//--Scrivo la costante per il numero di volte che devo stampare
+let n = 100
+//--Scrivo il ciclo for 
+for (let i= 0; i < n; i++) {
+    let num = i + 1
+    //console.log(num);
+
+    //scrivo le variabili per identificare quali siano i numeri multipli di 3 e di 5 
+    const multiplo3 = num % 3
+    const multiplo5 = num % 5
+    //--Se sono multipli sia di 3 che di 5 stampo "FizzBuzz" al posto del num
+    if (multiplo3 === 0 && multiplo5 === 0) {
+        
+        console.log("FizzBuzz")
+
+        //--Se sono multipli di 3 stampo "Fizz" al posto del num
+    } else if (multiplo3 === 0) {   
+        console.log("Fizz");
+
+        //--Se sono multipli di 5 stampo "Buzz" al posto del num
+    } else if (multiplo5 === 0) {
+        console.log("Buzz");
+
+        //--Se no stampo il numero
+    } else {
+        console.log(num)
+    }
+}
